59d69f5839dd75c8439cd1737b5b2aa06aebd781
Derell Nar <derell.nar@gmail.com>
2025-12-13 21:26:26 -0500

    Implement d10 Dice Roller

--- main.ts
+++ main.ts
@@ -1,54 +1,59 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting, ItemView, WorkspaceLeaf } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface VtmDicerollerSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: VtmDicerollerSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class VtmDicerollerPlugin extends Plugin {
+	settings: VtmDicerollerSettings;
 
 	async onload() {
 		await this.loadSettings();
 
+		// Register the view
+		this.registerView('vtm-dice-roller', (leaf: WorkspaceLeaf) => new DiceRollView(leaf));
+
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dices', 'VTM Dice Roller', (_evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			this.openDiceRollView();
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText('VTM Dice Roller');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'open-vtm-modal-simple',
+			name: 'Open VTM dice roller view (simple)',
 			callback: () => {
-				new SampleModal(this.app).open();
+				this.openDiceRollView();
 			}
 		});
+
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'vtm-editor-command',
+			name: 'VTM editor command',
 			editorCallback: (editor: Editor, _view: MarkdownView) => {
 				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				editor.replaceSelection('VTM Editor Command');
 			}
 		});
+
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: 'open-vtm-modal-complex',
+			name: 'Open VTM dice roller view (complex)',
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
 				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
@@ -56,7 +61,7 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						this.openDiceRollView();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -66,7 +71,7 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new VtmSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -82,6 +87,11 @@ export default class MyPlugin extends Plugin {
 
 	}
 
+	async openDiceRollView() {
+		const leaf = this.app.workspace.getRightLeaf(false);//.getLeaf(true);
+		if (leaf) { await leaf.setViewState({ type: 'vtm-dice-roller' }); }
+	}
+
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
@@ -91,26 +101,152 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class DiceRollView extends ItemView {
+	getViewType(): string {
+		return 'vtm-dice-roller';
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	getDisplayText(): string {
+		return 'VTM Dice Roller';
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	getIcon(): string {
+		return 'dices';
+	}
+
+	async onOpen() {
+		const container = this.containerEl.children[1];
+		container.empty();
+		container.setAttribute('style', 'padding: 10px;');
+
+		// View Title
+		container.createEl('h2', {text: 'Vampire the Masquerade V5 Dice Roller', cls: 'roller__title'});
+
+		// Input for number of dice
+		const inputContainer = container.createDiv();
+
+		// Input Total Dice Pool
+		inputContainer.createEl('label', {text: 'Total Dice Pool: '});
+		const dicePoolInput = inputContainer.createEl('input', {type: 'number', value: '5'});
+		dicePoolInput.setAttribute('min', '1');
+		dicePoolInput.setAttribute('max', '20');
+
+		// Line Break
+		inputContainer.createEl('br');
+
+		// Input Hunger Dice
+		inputContainer.createEl('label', {text: 'Hunger Dice: '});
+		const diceHungerInput = inputContainer.createEl('input', {type: 'number', value: '1'});
+		diceHungerInput.setAttribute('min', '1');
+		diceHungerInput.setAttribute('max', '5');
+
+		// Roll button
+		const rollButton = container.createEl('button', {text: 'Roll Dice'});
+		rollButton.addEventListener('click', () => {
+			const numHunger = parseInt(diceHungerInput.value);
+			const numRegularDice = parseInt(dicePoolInput.value) - numHunger;
+			if (isNaN(numRegularDice) || numRegularDice < 1) {
+				new Notice('Please enter a valid number of dice.');
+				return;
+			}
+
+			// Roll Regular Dice
+			const regularResults: (string | HTMLElement)[] = [];
+			for (let i = 0; i < numRegularDice; i++) {
+				var dieValue = Math.floor(Math.random() * 10) + 1
+				switch(dieValue) {
+					case 10:
+						const critImg = document.createElement('img');
+						critImg.src = './images/Crit.png'; // Path of Image
+						critImg.alt = 'Critical Success';
+						critImg.style.width = '20px';
+						critImg.style.height = '20px';
+						regularResults.push(critImg);
+						break;
+					case 6:
+					case 7:
+					case 8:
+					case 9:
+						const successImg = document.createElement('img');
+						successImg.src = './images/Success.png';
+						successImg.alt = 'Success';
+						successImg.style.width = '20px';
+						successImg.style.height = '20px';
+						regularResults.push(successImg);
+						break;
+					default:
+						const failImg = document.createElement('img');
+						failImg.src = './images/Teeth.png';
+						failImg.alt = 'Fail';
+						failImg.style.width = '20px';
+						failImg.style.height = '20px';
+						regularResults.push(failImg);
+				}
+			}
+
+			// Roll Hunger Dice
+			const hungerResults = [];
+			for (let i = 0; i < numHunger; i++) {
+				var diceValue = Math.floor(Math.random() * 10) + 1;
+				switch(diceValue) {
+					case 10:
+						hungerResults.push('Messy Crit');
+						break;
+					case 6:
+					case 7:
+					case 8:
+					case 9:
+						hungerResults.push('Success');
+						break;
+					case 1:
+						hungerResults.push('Bestial Fail');
+						break;
+					default:
+						hungerResults.push('Fail');
+				}
+			}
+
+			// Display results
+			const resultDiv = container.querySelector('.results') as HTMLElement;
+			if (resultDiv) {
+				resultDiv.empty();
+			} else {
+				container.createDiv({cls: 'results'});
+			}
+
+			// Setup Div
+			const resultsEl = container.querySelector('.results') as HTMLElement;
+			resultsEl.createEl('h4', {text: 'Regular Dice:'});
+			const regularP = resultsEl.createEl('p');
+			regularResults.forEach((item, index) => {
+				if (typeof item === 'string') {
+					regularP.appendChild(document.createTextNode(item));
+				} else {
+					regularP.appendChild(item);
+				}
+				if (index < regularResults.length - 1) {
+					regularP.appendChild(document.createTextNode(' '));
+				}
+			});
+
+			// Space between regular and hunger results
+			resultsEl.createEl('br');
+			
+			// Hunger Dice Results
+			resultsEl.createEl('h4', {text: 'Hunger Dice:'});
+			resultsEl.createEl('p', { text: hungerResults.join(', ')});
+		});
+	}
+
+	async onClose() {
+		// Cleanup if needed
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class VtmSettingTab extends PluginSettingTab {
+	plugin: VtmDicerollerPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: VtmDicerollerPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
